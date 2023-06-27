a0daa89aaa9425bbad251315fc9a3c9a15a13f3f
Jarod <jarod@studiotreble.com>
2023-06-27 09:48:10 +0100

    feat: color buttons

--- components/PlayButton.vue
+++ components/PlayButton.vue
@@ -1,15 +1,24 @@
 <template>
   <button
     class="h-full w-full p-2"
+    :class="{ 'bg-brand-yellow': isPlaying, 'bg-brand-green': !isPlaying }"
     @click="onClickToggle"
   >
-    <svg viewBox="0 0 10 10" fill="currentColor" class="h-full w-full">
+    <svg
+      viewBox="0 0 10 10"
+      stroke="currentColor"
+      stroke-width="1px"
+      class="h-full w-full overflow-visible fill-brand-background"
+    >
       <g v-if="isPlaying">
-        <path d="M1,1 h3 v8 h-3 Z" />
-        <path d="M6,1 h3 v8 h-3 Z" />
+        <path d="M1,1 h3 v8 h-3 Z" vector-effect="non-scaling-stroke" />
+        <path d="M6,1 h3 v8 h-3 Z" vector-effect="non-scaling-stroke" />
       </g>
       <g v-else>
-        <path d="M1.5,0.5 L9.5,5 L1.5,9.5 Z" />
+        <path
+          d="M1.5,0.5 L9.5,5 L1.5,9.5 Z"
+          vector-effect="non-scaling-stroke"
+        />
       </g>
     </svg>
   </button>
--- components/StopButton.vue
+++ components/StopButton.vue
@@ -1,11 +1,23 @@
 <template>
-  <button class="h-full w-full p-2" @click="onClickStop">
-    <svg viewBox="0 0 10 10" fill="currentColor" class="h-full w-full">
+  <button
+    class="h-full w-full p-2"
+    :class="{ 'bg-brand-red': isPlaying, 'bg-brand-green': !isPlaying }"
+    @click="onClickStop"
+  >
+    <svg
+      viewBox="0 0 10 10"
+      stroke="currentColor"
+      stroke-width="1px"
+      class="h-full w-full overflow-visible fill-brand-background"
+    >
       <g v-if="isPlaying">
-        <path d="M1,1 h8 v8 h-8 Z" />
+        <path d="M1,1 h8 v8 h-8 Z" vector-effect="non-scaling-stroke" />
       </g>
       <g v-else>
-        <path d="M1.5,0.5 L9.5,5 L1.5,9.5 Z" />
+        <path
+          d="M1.5,0.5 L9.5,5 L1.5,9.5 Z"
+          vector-effect="non-scaling-stroke"
+        />
       </g>
     </svg>
   </button>
