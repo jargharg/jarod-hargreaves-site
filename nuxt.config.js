12be310f5fc611f240c048db3e3f1171f23f5523
Jarod <jarod@studiotreble.com>
2023-06-29 10:24:37 +0100

    feat: favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -108,33 +108,23 @@ export default defineNuxtConfig({
         href: '/favicon.ico',
       },
       {
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicons/favicon.ico',
-      },
-      {
         rel: 'apple-touch-icon',
         sizes: '180x180',
-        href: '/favicons/apple-touch-icon.png',
-      },
-      {
-        rel: 'mask-icon',
-        color: '#000000',
-        href: '/favicons/safari-pinned-tab.svg',
+        href: '/apple-touch-icon.png',
       },
       {
         rel: 'icon',
         sizes: '32x32',
-        href: '/favicons/favicon-32x32.png',
+        href: '/favicon-32x32.png',
       },
       {
         rel: 'icon',
         sizes: '16x16',
-        href: '/favicons/favicon-16x16.png',
+        href: '/favicon-16x16.png',
       },
       {
         rel: 'manifest',
-        href: '/favicons/site.webmanifest',
+        href: '/site.webmanifest',
       },
     ],
   },
