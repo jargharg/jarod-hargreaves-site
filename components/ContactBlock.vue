e8af3677175edf7bcd76ae04d0a32c711cee7745
Jarod <jarod.hargreaves@gmail.com>
2023-07-05 10:17:17 +0100

    fix: don't show email address on hover on mobile

--- components/ContactBlock.vue
+++ components/ContactBlock.vue
@@ -1,22 +1,45 @@
 <template>
   <ContentBlock>
     <div class="grid grid-cols-2">
-      <CtaButton
-        href="mailto:jarod.hargreaves@gmail.com"
-        class="group block w-full text-brand-background font-bold bg-brand-green border-r border-brand-outline"
-      >
-        <span class="group-hover:hidden block"> Email </span>
-        <span class="group-hover:block hidden">
-          jarod.hargreaves@gmail.com
-        </span>
+      <CtaButton href="mailto:jarod.hargreaves@gmail.com" class="email">
+        <span class="email__label"> Email </span>
+        <span class="email__email"> jarod.hargreaves@gmail.com </span>
       </CtaButton>
 
       <CtaButton
         href="https://www.linkedin.com/in/jarodhargreaves/"
-        class="block w-full text-brand-background font-bold bg-brand-blue"
+        class="text-brand-background font-bold bg-brand-blue"
       >
         LinkedIn
       </CtaButton>
     </div>
   </ContentBlock>
 </template>
+
+<style lang="scss" scoped>
+.email {
+  $email: &;
+  @apply text-brand-background font-bold bg-brand-green border-r border-brand-outline;
+
+  &__label {
+    @apply block;
+  }
+
+  &__email {
+    @apply hidden;
+  }
+
+  @media (hover: hover) {
+    &:hover,
+    &:focus {
+      #{$email}__label {
+        @apply hidden;
+      }
+
+      #{$email}__email {
+        @apply block;
+      }
+    }
+  }
+}
+</style>
